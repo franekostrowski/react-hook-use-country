b05af48f1ac10dd761d6b289f9365b44577753aa
franciszekostrowski <franekostrowski@protonmail.com>
2023-02-18 23:27:39 +0100

    feat: add listOfCountries export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,12 @@ import getCountryCode from "./getCountryCode";
 import getCountryData from "./getCountryData";
 import getCountryFlag from "./getCountryFlag";
 import getCountryName from "./getCountryName";
+import listOfCountries from "./listOfCountries";
 
-export { getCountryData, getCountryCode, getCountryFlag, getCountryName };
+export {
+  getCountryData,
+  getCountryCode,
+  getCountryFlag,
+  getCountryName,
+  listOfCountries
+};
